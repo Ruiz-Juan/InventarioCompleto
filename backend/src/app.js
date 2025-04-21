989c165d0227998d5f9ec285959fdb21ade96282
Juan Ruiz <ruizjuan.2004@gmail.com>
2025-04-20 22:51:42 -0500

    Guardar Registros - Método HTTP POST

---
+++ backend/src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors"
+import categoriaRoutes from "./routes/categorias.routes.js"
+
+const app = express();
+
+app.set("port", 5000)
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use("/api/categorias", categoriaRoutes)
+
+export default app;
---
+++ backend/src/controllers/categoria.controllers.js
@@ -0,0 +1,33 @@
+import getConnection from "./../db/database.js"
+
+const getCategorias = async (req, res) => {
+    try {
+        const connection = await getConnection();
+        const result = await connection.query("SELECT CategoriaID, CategoriaNombre, Descripcion, Imagen FROM categorias")
+        res.json(result);
+    } catch (error) {
+        console.error("ERROR 500");
+    }
+}
+
+const postCategorias = async (req, res) => {
+    try {
+        const { CategoriaNombre, Descripcion, Imagen } = req.body;
+
+        const category = { CategoriaNombre, Descripcion, Imagen }
+
+        const connection = await getConnection();
+
+        const result = await connection.query("INSERT INTO categorias SET ?", category)
+
+        res.json(result)
+
+    } catch (error) {
+        console.error("ERROR 500");
+    }
+}
+export const methodHTTP = {
+    getCategorias,
+    postCategorias
+}
+
