7cb3a77daa675b59fe30d3b934efb1ecfbaf5cbe
Juan Ruiz <ruizjuan.2004@gmail.com>
2025-04-27 21:05:43 -0500

    Actualizacion Segundo Parcial

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors"
 import categoriaRoutes from "./routes/categorias.routes.js"
+import empleadoRoutes from "./routes/empleados.routes.js";
+import clienteRoutes from "./routes/clientes.routes.js";
+import productoRoutes from "./routes/productos.routes.js";
 
 const app = express();
 
@@ -10,6 +13,9 @@ app.use(express.json());
 
 app.use(cors());
 
-app.use("/api/categorias", categoriaRoutes)
+app.use("/api/categorias", categoriaRoutes);
+app.use("/api/empleados", empleadoRoutes);
+app.use("/api/clientes", clienteRoutes);
+app.use("/api/productos", productoRoutes);
 
 export default app;
